38000c76b4339ec26e2652e965a4895b6cb75533
lightlessun <47335065+lightlessun@users.noreply.github.com>
2019-11-09 16:53:18 -0500

    health script added, made fuel meaningful

---
+++ Space-Game/Assets/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+    private const float DEAD = 0.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = 100.0f;
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <= DEAD)
+        {
+            //Dead code here
+        }
+    }
+}
--- Space-Game/Assets/PlayerMovement.cs
+++ Space-Game/Assets/PlayerMovement.cs
@@ -9,41 +9,75 @@ public class PlayerMovement : MonoBehaviour
     public GameObject player_cam;
     private Rigidbody rb;
 
-    public float gunThrust;
-    public float fuelThrust;
-    public float fuelTotal;
+    public float gunRecoil;
+    public float playerSpeed;
+    public float maxFuel;
     public float fuelConsumption;
     private float doubleTapTimer;
+    private float fuelRegen;
+    public float currentFuel;
     public float doubleTapDelay;
     private bool hasDashed = false;
+    private const float EMPTY = 0f;
+
+    private const string JUMP = "Jump";
+    private const string SHIFT = "Descend";
 
     // Start is called before the first frame update
     void Start()
     {
-        fuelTotal = 100f;
+        maxFuel = 100f;
         fuelConsumption = 0.2f;
+        fuelRegen = 0.05f;
+        currentFuel = maxFuel;
         rb = player_body.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Weapon movement logic
         if (Input.GetButtonDown("Fire1"))
         {
-            rb.AddForce(player_cam.transform.forward * -1 * gunThrust);
+            rb.AddForce(player_cam.transform.forward * -1 * gunRecoil);
         }
-        if (Input.GetButton("Jump"))
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //lateral movement logic start
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        
+        //if space pressed and fuel is not empty
+        if (Input.GetButton(JUMP) && currentFuel > EMPTY )
         {
+
             Debug.Log("space");
-            Vector3 forceToAdd = new Vector3(0, fuelThrust, 0);
+            Vector3 forceToAdd = new Vector3(0, playerSpeed, 0);
             rb.AddForce(forceToAdd);
+
+            //fuel is zero if no more fuel left, otherwise remove fuel
+            currentFuel = currentFuel - fuelConsumption > EMPTY ? currentFuel - fuelConsumption : EMPTY;
         }
-        if (Input.GetButton("Descend"))
+
+        //if pressed pressed and fuel is not empty
+        else if (Input.GetButton("Descend") && currentFuel > EMPTY)
         {
             Debug.Log("shift");
-            Vector3 forceToAdd = new Vector3(0, fuelThrust * -1, 0);
+            Vector3 forceToAdd = new Vector3(0, playerSpeed * -1, 0);
             rb.AddForce(forceToAdd);
+
+            currentFuel = currentFuel - fuelConsumption > EMPTY ? currentFuel - fuelConsumption : EMPTY;
+
         }
+        //if shift and space is not pressed
+        else if (!Input.GetButton(JUMP) && !Input.GetButton("Descend"))
+        {
+            currentFuel = currentFuel + fuelRegen < maxFuel ? currentFuel += fuelRegen : maxFuel;
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //lateral movement logic end
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         if (Input.GetButtonDown("Left"))
         {
             doubleTapTimer = Time.time + doubleTapDelay;
@@ -54,13 +88,13 @@ public class PlayerMovement : MonoBehaviour
                 if (Input.GetButtonDown("Left") && Time.time < doubleTapTimer)
                 {
                     Debug.Log("left with dash");
-                    rb.AddForce(player_cam.transform.right * -1 * fuelThrust * fuelThrust);
+                    rb.AddForce(player_cam.transform.right * -1 * playerSpeed * playerSpeed);
                     hasDashed = true;
                 }
                 else
                 {
                     Debug.Log("left no dash");
-                    rb.AddForce(player_cam.transform.right * -1 * fuelThrust);
+                    rb.AddForce(player_cam.transform.right * -1 * playerSpeed);
                 }
             }
         }
@@ -74,13 +108,13 @@ public class PlayerMovement : MonoBehaviour
                 if (Input.GetButtonDown("Right") && Time.time < doubleTapTimer)
                 {
                     Debug.Log("right with dash");
-                    rb.AddForce(player_cam.transform.right * fuelThrust * fuelThrust);
+                    rb.AddForce(player_cam.transform.right * playerSpeed * playerSpeed);
                     hasDashed = true;
                 }
                 else
                 {
                     Debug.Log("right no dash");
-                    rb.AddForce(player_cam.transform.right * fuelThrust);
+                    rb.AddForce(player_cam.transform.right * playerSpeed);
                 }
             }
         }
@@ -94,13 +128,13 @@ public class PlayerMovement : MonoBehaviour
                 if (Input.GetButtonDown("Forward") && Time.time < doubleTapTimer)
                 {
                     Debug.Log("forward with dash");
-                    rb.AddForce(player_cam.transform.forward * fuelThrust * fuelThrust);
+                    rb.AddForce(player_cam.transform.forward * playerSpeed * playerSpeed);
                     hasDashed = true;
                 }
                 else
                 {
                     Debug.Log("forward no dash");
-                    rb.AddForce(player_cam.transform.forward * fuelThrust);
+                    rb.AddForce(player_cam.transform.forward * playerSpeed);
                 }
             }
         }
@@ -114,35 +148,35 @@ public class PlayerMovement : MonoBehaviour
                 if (Input.GetButtonDown("Backward") && Time.time < doubleTapTimer)
                 {
                     Debug.Log("backward with dash");
-                    rb.AddForce(player_cam.transform.forward * -1 * fuelThrust * fuelThrust);
+                    rb.AddForce(player_cam.transform.forward * -1 * playerSpeed * playerSpeed);
                     hasDashed = true;
                 }
                 else
                 {
                     Debug.Log("backward no dash");
-                    rb.AddForce(player_cam.transform.forward * -1 * fuelThrust);
+                    rb.AddForce(player_cam.transform.forward * -1 * playerSpeed);
                 }
             }
         }
         if (Input.GetButton("Left"))
         {
             Debug.Log("left");
-            rb.AddForce(player_cam.transform.right * -1 * fuelThrust);
+            rb.AddForce(player_cam.transform.right * -1 * playerSpeed);
         }
         if (Input.GetButton("Right"))
         {
             Debug.Log("right");
-            rb.AddForce(player_cam.transform.right * fuelThrust);
+            rb.AddForce(player_cam.transform.right * playerSpeed);
         }
         if (Input.GetButton("Forward"))
         {
             Debug.Log("right");
-            rb.AddForce(player_cam.transform.forward * fuelThrust);
+            rb.AddForce(player_cam.transform.forward * playerSpeed);
         }
         if (Input.GetButton("Backward"))
         {
             Debug.Log("right");
-            rb.AddForce(player_cam.transform.forward * -1 * fuelThrust);
+            rb.AddForce(player_cam.transform.forward * -1 * playerSpeed);
         }
 
     }
