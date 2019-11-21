fef652bfad3bdf8f2d6fa5c3acdd77a643f68924
VenoShulk <57199800+Venoshulk@users.noreply.github.com>
2019-11-20 20:51:54 -0500

    Commit Base Projectile

---
+++ Space-Game/Assets/PlayerWeapons.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapons : MonoBehaviour
+{
+    public class Weapon
+    {
+        public Animation shootAnim;
+        public GameObject projectile;
+
+        public float weaponDuration;
+        public float shootDelay;
+        private float shootTimer;
+        private float shootOffSet = 1f;
+
+        public bool isDefault;
+
+        public void fireWeapon(GameObject defaultCam)
+        {
+            if (Time.time >= shootTimer)
+            {
+                shootTimer = Time.time + shootDelay;
+                
+                //create object (projectile) in front of player by using the camera position
+                Instantiate(projectile, defaultCam.transform.position + defaultCam.transform.forward * shootOffSet, Quaternion.identity);
+            }
+            else
+            {
+                Debug.Log("Wait!");
+            }
+        }
+    }
+
+    public GameObject defaultBullet;
+    public GameObject defaultCam;
+
+    Weapon defaultWeapon = new Weapon { isDefault = true, shootDelay = 0.2f};
+    Weapon currentWeapon;
+    
+
+    private void Start()
+    {
+        defaultWeapon.projectile = defaultBullet;
+        currentWeapon = defaultWeapon;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1")){
+            currentWeapon.fireWeapon(defaultCam);
+        }
+    }
+}
---
+++ Space-Game/Assets/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody rb;
+    public float thrust = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * thrust);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
