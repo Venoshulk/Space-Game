3de79d6516205fa12d5ed6fa0259d9d17b97ba4a
VenoShulk <arthurmousatov@gmail.com>
2019-12-12 20:18:56 -0500

    Added Proper Bullet Trajectories
    -Added comments
    -Added proper bullet direction (Now bullets will come out at the correct spot)

--- Space-Game/Assets/PlayerWeapons.cs
+++ Space-Game/Assets/PlayerWeapons.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class PlayerWeapons : MonoBehaviour
 {
-    public class Weapon
+    public class Weapon //Class that each weapon is based from
     {
-        public Animation shootAnim;
-        public GameObject projectile;
+        public Animation shootAnim; //Shooting Animation
+        public GameObject weaponGraphic; //Weapon Graphic
+        public GameObject projectile;  //Weapon Bullet
 
-        public float weaponDuration;
-        public float shootDelay;
-        private float shootTimer;
-        private float shootOffSet = 1f;
+        public float weaponDuration; //The duration of the powerup
+        public float shootDelay; //The delay between shots
+        private float shootTimer; //The time you can shoot again (used with shoot delay)
+        private float shootOffSet = 1f; //The position offset of the bullet
 
-        public bool isDefault;
+        //Set specific model parameters
+        public Weapon(float shootingDelay)
+        {
+            shootDelay = shootingDelay;
+        }
 
         public void fireWeapon(GameObject defaultCam)
         {
             if (Time.time >= shootTimer)
             {
+                Debug.Log("Shoot");
+                //Reset the shoot timer
                 shootTimer = Time.time + shootDelay;
                 
                 //create object (projectile) in front of player by using the camera position
-                Instantiate(projectile, defaultCam.transform.position + defaultCam.transform.forward * shootOffSet, Quaternion.identity);
+                Instantiate(projectile, defaultCam.transform.position + defaultCam.transform.forward * shootOffSet, defaultCam.transform.rotation);
             }
             else
             {
@@ -35,7 +42,7 @@ public class PlayerWeapons : MonoBehaviour
     public GameObject defaultBullet;
     public GameObject defaultCam;
 
-    Weapon defaultWeapon = new Weapon { isDefault = true, shootDelay = 0.2f};
+    Weapon defaultWeapon = new Weapon(0.02f);
     Weapon currentWeapon;
     
 
