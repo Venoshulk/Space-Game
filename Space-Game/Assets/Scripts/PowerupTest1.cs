996a4baa7190fdb4473986efc8edb0c71d68d237
ZacharyTherrien <ztherrien.work@gmail.com>
2019-12-21 23:40:46 -0500

    Powerups development has begun
    Created the basis for a basic poewrup class. Also renamed the powerup script to have a more appropriate name as it should have retained to the weapons.

---
+++ Space-Game/Assets/Scripts/PowerupTest1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupTest1 : Powerups
+{
+    //Properties
+    private int _Time = 5;
+
+    //Constructor
+    public PowerupTest1()
+    {   SetTimer(_Time);
+    }
+
+    //Check for collision, if so, apply power up.
+    void OnTriggerEnter(Collider other)
+    {   Debug.Log("Is Player?");
+        if(other.CompareTag("Player"))          //Check collision by collider's tag.
+        {   Debug.Log("Power up!");
+            StartCoroutine(ApplyEffects());     //StartCoroutine is required in order to call IEnumerator.
+        }
+    }
+
+    //Apply powerup for X time, and then remove effects.
+    IEnumerator ApplyEffects()
+    {   Debug.Log("Powered up!");
+        yield return new WaitForSeconds(GetTimer());        //Before removing buffs, wait.
+        Debug.Log("Powered up off!");
+        //Destroy powerup on field.
+        Destroy(gameObject);                                //Remove the object.
+    } 
+}
---
+++ Space-Game/Assets/Scripts/Powerups.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerups : MonoBehaviour
+{ 
+    //Properties
+    private int _Timer = 1;
+    
+    //Get & Set functions
+    public void SetTimer(int Timer)
+    {   _Timer = Timer;
+    }
+
+    public int GetTimer()
+    {   return _Timer;
+    }
+}
