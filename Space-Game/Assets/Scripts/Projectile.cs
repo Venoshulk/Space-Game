86b79290a02882d25bb30ae7c9cd028a48cf466f
Venoshulk <arthurmousatov@gmail.com>
2019-12-21 21:53:23 -0500

    Fixed bullet rotation

--- Space-Game/Assets/Scripts/Projectile.cs
+++ Space-Game/Assets/Scripts/Projectile.cs
@@ -5,17 +5,11 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     Rigidbody rb;
-    public float thrust = 10f;
+    public float thrust;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.AddForce(transform.forward * thrust);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
