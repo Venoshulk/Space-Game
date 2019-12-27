2c72b98f96b158f2d1ddbdaf652f975002b2bfee
ddizzlemyfizzle <57300689+ddizzlemyfizzle@users.noreply.github.com>
2019-12-27 16:13:12 -0500

    gravity now interacts with the player. Planets can even pull players into a weird sort of orbit. It's kinda funky right now

---
+++ Space-Game/Assets/PlanetGravity2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGravity2 : MonoBehaviour
+{
+
+
+
+public int GravForce = 9000;
+
+public GameObject ObjectToPull;
+// Start is called before the first frame update
+void Start()
+{
+
+}
+
+private void OnTriggerStay(Collider other)
+{
+
+    if (other.gameObject == ObjectToPull)
+    {
+        Rigidbody PlayerRB = other.gameObject.GetComponent<Rigidbody>();
+
+        PlayerRB.AddForce((transform.position - ObjectToPull.transform.position).normalized * GravForce * Time.smoothDeltaTime);
+        //Debug.Log("penis");
+    }
+
+}
+
+// Update is called once per frame
+  void Update()
+  {
+
+  }
+
+}
