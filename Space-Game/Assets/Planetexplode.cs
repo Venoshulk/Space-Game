0fd2dae96dc95511f2f21b8ef4a426535e71a1dc
ddizzlemyfizzle <57300689+ddizzlemyfizzle@users.noreply.github.com>
2020-03-13 20:19:12 -0400

    A script that makes planet go boom boom
    boom

---
+++ Space-Game/Assets/Planetexplode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planetexplode : MonoBehaviour
+{
+    public int planetHealth = 35;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planetHealth = 35;
+        
+
+    }
+
+    private void OnTriggerStay(Collider bullet)
+    {
+        if (planetHealth<0)
+        {
+            BlowUp();
+        }
+    }
+
+
+    void BlowUp ()
+    {
+        Object.Destroy(gameObject);
+        
+    }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
