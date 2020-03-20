84356e602af7e642e9cb49445f5fc196c95ac286
Venoshulk <arthurmousatov@gmail.com>
2020-03-20 19:20:01 -0400

    Added Base Bull

---
+++ Space-Game/Assets/BullScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BullScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 force = Vector3(1, 1, 1);
+        rb.AddForce();
+    }
+}
