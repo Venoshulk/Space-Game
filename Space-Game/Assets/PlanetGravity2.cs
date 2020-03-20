6b5b0da4d1d7db7d5fd23f7ec91b3641203f8219
ddizzlemyfizzle <57300689+ddizzlemyfizzle@users.noreply.github.com>
2020-03-20 19:40:21 -0400

    Gravity and planet explode now work on both planets

---
+++ Space-Game/Assets/PlanetGravity2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetGravity2 : MonoBehaviour
+{
+    public int GravForce;
+
+    public GameObject ObjectToPull;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == ObjectToPull)
+        {
+            Rigidbody PlayerRB = other.gameObject.GetComponent<Rigidbody>();
+
+            PlayerRB.AddForce((transform.position - ObjectToPull.transform.position).normalized * GravForce * Time.smoothDeltaTime);
+            //Debug.Log("penis");
+        }
+    }
+}
---
+++ Space-Game/Assets/Scripts/Projectile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody rb;
+    public float thrust;
+    public float duration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        OnInstance();
+        StartCoroutine(BulletDeath());
+    }
+    //On collision with another object
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            //Cause damage -> Not yet implemented
+            DestroyBullet();
+        }
+        else
+        {
+            DestroyBullet();
+        }
+    }
+    //On creation function
+    void OnInstance()
+    {
+        rb.AddForce(transform.forward * thrust);
+    }
+    //Waits for a duration then executes a destroy function
+    IEnumerator BulletDeath()
+    {
+        yield return new WaitForSeconds(duration);
+        DestroyBullet();
+    }
+
+    void DestroyBullet()
+    {
+        Object.Destroy(gameObject);
+    }
+}
