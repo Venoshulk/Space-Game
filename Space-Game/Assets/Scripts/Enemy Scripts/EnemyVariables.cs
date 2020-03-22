cda3e02cf5772b86705f9866b5e021bef1b31597
Venoshulk <arthurmousatov@gmail.com>
2020-03-22 15:24:13 -0400

    Merge branch 'Enemy' of https://github.com/Venoshulk/Space-Game into Enemy

--- Space-Game/Assets/Scripts/Enemy Scripts/EnemyVariables.cs
+++ Space-Game/Assets/Scripts/Enemy Scripts/EnemyVariables.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class EnemyVariables : Health
 {
+    private int _score;
+
     void Start()
     {
         maxHealth = 100.0f;
         currentHealth = maxHealth;
+        _score = 100;
     }
     void Update()
     {
@@ -18,8 +21,12 @@ public class EnemyVariables : Health
         if (currentHealth <= DEAD)
         {
             //Dead code here
+            PlayerScore.ModifyScore(_score);
+            Debug.Log("Score after enemy: " + PlayerScore.GetScoreValue());
             Death();
+            Debug.Log("Retired!!!");
             Debug.Log("AWOOOOOOOOOOOOOGA");
+            Debug.Log("Log messages are fun, yeet.");
             Debug.Log("Retired");
         }
     }
